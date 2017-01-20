cb197ab6911929fb1245c9b8ce0be7b6168af573
Daniel Watkins <daniel.watkins@canonical.com>
2017-01-20 11:39:28 +0000

    Ensure cloud_config output is valid yaml

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import yaml
+
 import ubuntu_standalone_builder
 
 
@@ -7,3 +9,8 @@ class TestWriteCloudConfig(object):
         output_file = tmpdir.join('output.yaml')
         ubuntu_standalone_builder._write_cloud_config(output_file.strpath)
         assert output_file.check()
+
+    def test_written_output_is_yaml(self, tmpdir):
+        output_file = tmpdir.join('output.yaml')
+        ubuntu_standalone_builder._write_cloud_config(output_file.strpath)
+        yaml.load(output_file.read())
